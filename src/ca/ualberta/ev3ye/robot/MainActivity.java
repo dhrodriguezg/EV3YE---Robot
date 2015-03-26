d6a3c23b50f4c281e9e18cfc90a47fa961e5e02e
dhrodriguezg <dh.rodriguezg@gmail.com>
2015-03-25 23:42:46 -0600

    Motors Working with BT
    code;leftpower,rightpower

--- src/ca/ualberta/ev3ye/robot/MainActivity.java
+++ src/ca/ualberta/ev3ye/robot/MainActivity.java
@@ -1,154 +1,146 @@
 package ca.ualberta.ev3ye.robot;
 import java.io.*;
 
+import lejos.hardware.Audio;
 import lejos.hardware.Bluetooth;
 import lejos.hardware.Button;
-import lejos.hardware.lcd.LCD;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.hardware.motor.NXTMotor;
 import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.Port;
 import lejos.remote.nxt.*;
-import lejos.robotics.EncoderMotor;
-import lejos.robotics.navigation.DifferentialPilot;
-import lejos.utility.Delay;
 
-public class MainActivity {
+public class MainActivity { 
+
+	public final static int CONTROLLER_OPERATION = 1; //Using gamepad
+	public final static int TOUCH_OPERATION = 2; //Using touchscreen
+	public final static int MOTION_OPERATION = 3; //Using acelerometers
+	public final static int VISUAL_OPERATION = 4; //Using visual servoig
+	public final static int EXIT = -1; //Close communication
 	
-	DifferentialPilot pilot;
-
-	public static DataOutputStream dataOut;
-	public static DataInputStream dataIn;
-	//public static USBConnection USBLink;
-	private static NXTConnection BTLink;
-	public static int speed = 50, turnSpeed = 50, speedBuffer, speedControl;
-	public static int commandValue, transmitReceived;
-	public static boolean[] control = new boolean[6];
-	public static boolean[] command = new boolean[6];
+	private boolean forceExit = false;
+	
+	public final static Port motorL = MotorPort.A;
+	public final static Port motorR = MotorPort.D;
+	
+	private EV3MediumRegulatedMotor reMotorL = null;
+	private EV3MediumRegulatedMotor reMotorR = null;
+	
+	private NXTMotor enMotorL = null;
+	private NXTMotor enMotorR = null;
 
 	public static void main(String[] args) {
-		MainActivity traveler = new MainActivity();
-		traveler.connect();
-		traveler.transferData();
-		//traveler.moveBrick();
-		
-	}// End main
-
-	public void connect() {
-		System.out.println("Listening");
-		BTLink = Bluetooth.getNXTCommConnector().waitForConnection(10000, NXTConnection.RAW);
-		//dataIn = BTLink.openDataInputStream();
-		System.out.println("Connected :')");
-	}
-	
-	public void transferData(){
-		
-		DataInputStream dis=BTLink.openDataInputStream();
-		String word = "NOPE";
-		try {
-			word=dis.readUTF();
-			System.out.println("Word: "+word);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		System.out.println("Exit...");
+		MainActivity robot = new MainActivity();
+		robot.waitForConnections();
+		robot.waitExit();
 	}
 	
 	
+	private void waitForConnections(){
+		System.out.println("EV3YE Waiting...");
+		Thread thread = new Thread() {
+            public void run() {
+            	
+            	for (int i=0;i<10;i++){
+            		NXTConnection btLink = Bluetooth.getNXTCommConnector().waitForConnection(10000, NXTConnection.RAW);
+            		try {
+						manageConnection(btLink);
+					} catch (IOException | InterruptedException e) {
+						e.printStackTrace();
+					}
+            		if(forceExit)
+            			break;
+        		}
+            	
+            }
+        };
+        thread.start();
+	}
 	
-	@SuppressWarnings("resource")
-	public void moveBrick(){
-		int powerA = 0;
-		int powerB = 0;
-		int addedPowerA=0;
-		int addedPowerB=0;
+	private void manageConnection(NXTConnection btLink) throws IOException, InterruptedException{
+		boolean greeting = true;
+		boolean isTransmiting = true;
 		
-		EncoderMotor motorA =new NXTMotor (MotorPort.A);
-		EncoderMotor motorB =new NXTMotor (MotorPort.B);
-		motorA.resetTachoCount();
-		motorB.resetTachoCount();
+		DataInputStream dataIn=btLink.openDataInputStream();
+		DataOutputStream dataOut=btLink.openDataOutputStream();
 		
-		while (!Button.DOWN.isDown()) {
-
-			try {
-				
-				
-				int data = dataIn.readInt();
-				
-				if (data==10){ //forward pressed
-					powerA += 75;
-					powerB += 75;
-				}
-				if (data==-10){ //forward released
-					powerA -= 75;
-					powerB -= 75;
-				}
-				if (data==20){ //backward pressed
-					powerA -= 75;
-					powerB -= 75;
-				}
-				if (data==-20){ //backward released
-					powerA += 75;
-					powerB += 75;
-				}
-				if (data==30){ //left pressed
-					addedPowerA-=30;
-					addedPowerB+=30;
-				}
-				if (data==-30){ //left released
-					addedPowerA+=30;
-					addedPowerB-=30;
-				}
-				if (data==40){ //right pressed
-					addedPowerA+=30;
-					addedPowerB-=30;
-				}
-				if (data==-40){ //right released
-					addedPowerA-=30;
-					addedPowerB+=30;
-				}
-				
-				int totalA=powerA+addedPowerA;
-				int totalB=powerB+addedPowerB;
-				
-				if (data==50){ //right released
-					totalA = 0;
-					totalB = 0;
-					
-					powerA = 0;
-					powerB = 0;
-					
-					addedPowerA = 0;
-					addedPowerB = 0;
-				}
-				
-				if (totalA == 0) {
-					motorA.stop();
-				}
-				else if(totalA<0){
-					motorA.setPower(-totalA);
-					motorA.backward();
-				}else{
-					motorA.setPower(totalA);
-					motorA.forward();
-				}
-				
-				if (totalB == 0) {
-					motorB.stop();
-				}
-				else if(totalB<0){
-					motorB.setPower(-totalB);
-					motorB.backward();
-				}else{
-					motorB.setPower(totalB);
-					motorB.forward();
-				}
-				
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				//e.printStackTrace();
+		while(isTransmiting){
+			String commands=dataIn.readUTF();
+			String[] command=commands.split(";");
+			
+			if(greeting && command[0].equals("Are you Robot?")){
+				Audio audio = LocalEV3.get().getAudio();
+				audio.systemSound(Audio.ASCENDING);
+				dataOut.writeBoolean(true);
+				dataIn.close();
+				dataOut.close();
+				return;
 			}
+			greeting=false;
+			int operation = Integer.parseInt(command[0]);
+			if (operation==EXIT){
+				isTransmiting=false;
+			}
+			else if(operation==VISUAL_OPERATION){
+				turnMotors();
+			}else{
+				if(command.length>2)
+				moveMotors(Integer.parseInt(command[1]),Integer.parseInt(command[2]));
+			}
+			dataOut.writeBoolean(true);
+			Thread.sleep(5);
+			
+			if(forceExit)
+				break;
 		}
+		dataIn.close();
+		dataOut.close();
 		
 	}
-
-}// NXTtr Class
-
+	
+	private void moveMotors(int powerL, int powerR){
+		
+		if(reMotorL!=null)
+			reMotorL.close();
+		if(reMotorR!=null)
+			reMotorR.close();
+		
+		if(enMotorL==null){
+			enMotorL = new NXTMotor(motorL);
+			enMotorL.resetTachoCount();
+		}
+		if(enMotorR==null){
+			enMotorR = new NXTMotor(motorR);
+			enMotorR.resetTachoCount();
+		}
+		
+		enMotorL.setPower(powerL);
+		enMotorR.setPower(powerR);
+		enMotorL.forward();
+		enMotorR.forward();
+	}
+	
+	private void turnMotors(){
+		if(enMotorL!=null)
+			enMotorL.close();
+		if(enMotorR!=null)
+			enMotorR.close();
+		
+		if(reMotorL==null){
+			reMotorL = new EV3MediumRegulatedMotor(motorL);
+			reMotorL.resetTachoCount();
+		}
+		if(reMotorR==null){
+			reMotorR = new EV3MediumRegulatedMotor(motorR);
+			reMotorR.resetTachoCount();
+		}
+		//TODO visual servoing here :S
+	}
+	
+	private void waitExit(){
+		Button.waitForAnyPress();
+		forceExit = true;
+	}
+	
+}
